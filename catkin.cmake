e68a7328a68aa3337c3829c465fac83497032236
Mike Koval <mkoval@cs.cmu.edu>
2014-10-11 15:48:58 -0400

    Added openrave_catkin dependency.

--- catkin.cmake
+++ catkin.cmake
@@ -1,16 +1,13 @@
 cmake_minimum_required(VERSION 2.8.3)
 
-find_package(catkin REQUIRED owd_msgs roscpp roslib)
+find_package(catkin REQUIRED owd_msgs roscpp roslib openrave_catkin)
 catkin_package()
-
-find_package(OpenRAVE REQUIRED)
-set(OpenRAVE_LIBRARY_SUFFIX "0.9")
 
 include(FindPkgConfig)
 pkg_check_modules(YamlCpp REQUIRED yaml-cpp)
 
 include_directories(
-    include/${PROJECT_NAME}
+    "include/${PROJECT_NAME}"
     ${OpenRAVE_INCLUDE_DIRS}
     ${YamlCpp_INCLUDE_DIRS}
     ${catkin_INCLUDE_DIRS}
@@ -22,7 +19,7 @@ link_directories(
 )
 add_definitions(-DNO_MAC_TRAJECTORY)
 
-add_library(${PROJECT_NAME}_plugin
+openrave_plugin("${PROJECT_NAME}_plugin"
     src/BHController.cpp
     src/BHTactileSensor.cpp
     src/BarrettFTSensor.cpp
@@ -30,19 +27,10 @@ add_library(${PROJECT_NAME}_plugin
     src/OWDController.cpp
     src/AllOWDPlugins.cpp
 )
-target_link_libraries(${PROJECT_NAME}_plugin
-    ${OpenRAVE_LIBRARIES}
+target_link_libraries("${PROJECT_NAME}_plugin"
     ${YamlCpp_LIBRARIES}
     ${catkin_LIBRARIES}
 )
-set_target_properties(${PROJECT_NAME}_plugin PROPERTIES
-    PREFIX ""
-    LIBRARY_OUTPUT_DIRECTORY "${CATKIN_DEVEL_PREFIX}/${CATKIN_GLOBAL_LIB_DESTINATION}/openrave-${OpenRAVE_LIBRARY_SUFFIX}"
-)
-add_dependencies(${PROJECT_NAME}_plugin
+add_dependencies("${PROJECT_NAME}_plugin"
     ${catkin_EXPORTED_TARGETS}
-)
-
-install(TARGETS ${PROJECT_NAME}_plugin
-    LIBRARY DESTINATION "${CATKIN_PACKAGE_LIB_DESTINATION}/openrave-${OpenRAVE_LIBRARY_SUFFIX}"
 )
